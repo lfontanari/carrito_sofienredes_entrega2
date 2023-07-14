b151ec4c38556b9deffe9d1df17b42510fe12f12
Lorena Fontanari <lorenafontanari@gmail.com>
2023-07-14 00:05:24 -0300

    actualizo tomar productos del json

--- JS/app.js
+++ JS/app.js
@@ -1,75 +1,130 @@
-class Producto {
 
-    constructor(id, nombre, precio, description){
-      this.id = id;
-      this.nombre = nombre;
-      this.precio = precio;
-      this.description = description;
+class TiendaSofi {
+  
+  constructor() {
+    const productContainer = document.querySelector('#product-container');
+    this.carrito = [];
+   
+    this.productos = [];
+
+    this.loadProductos();
+
+    this.loadCartsHtml();
+
+    const buttons = document.querySelectorAll('.btn.btn-primary');
+    buttons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        const nombre = event.target.id;
+        const producto = this.getProductByName(nombre);
+        this.getProductPrice(producto);
+      });
+    });
+  }
+
+
+
+ loadProductos() {
+    const urlProductos = './json/productos.json';
+    fetch(urlProductos)
+      .then(response => response.json())
+      .then(data => {
+        this.productos = data;
+      })
+      .catch(error => {
+        console.error('Error al cargar los productos:', error);
+      });
+  }
+
+  // cargar los productos leidos del jason en las tarjetas del html
+  loadCartsHtml() {
+    // recorro el arreglo de productos y creo las cartas para mostrar en el front
+    this.productos.forEach( producto => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add("producto");
+        productDiv.style.backgroundColor='#FFFFFF';
+        //innerHTML
+        productDiv.innerHTML = `
+         <div class='card'>
+          <h3>${producto.ID}</h3>
+          <h1>${producto.nombre}</h1>
+          <p>${producto.precio}</p>
+          <p>${producto.categoria}</p>
+          <button class="agregar-carrito">Agregar al Carrito</button>
+          <button class="eliminar-carrito">Eliminar del Carrito</button>
+        </div> `;
+
+        productContainer.appendChild(productDiv);
+
+        const addButton = productDiv.querySelector('.agregar-carrito');
+        addButton.addEventListener('click', () => {
+          console.log("Se agrego al carrito!", producto.nombre);
+          addCarrito(producto);
+        });
+      
+      
+        const deleteButton = productDiv.querySelector('.eliminar-carrito');
+        addButton.addEventListener('click', () => {
+          console.log("Se saco del carrito!", producto.nombre);
+          eliminarCarrito(producto.ID);
+        });
+      })
+    }
+
+    
+    getProductByName(nombre) {
+      return this.productos.find(producto => producto.nombre === nombre);
+    }
+
+    getProductPrice(producto) {
+      const  nombre = producto;
+
+      Swal.fire({
+        title: '¿Desea comprar este producto?',
+        text: `No podrá revertir esta acción para el producto ${nombre}!`,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, comprar',
+        cancelButtonText: 'No, cancelar',
+        reverseButtons: true,
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      }).then((result) => {
+        if (result.isConfirmed) {
+          
+          this.carrito.push({ producto: nombre });
+          this.showAlert(`El producto ${nombre} agregado al carrito de compras.`);
+          console.log(`Producto ${nombre} agregado al carrito.`);
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          this.showAlert("La acción ha sido cancelada.");
+        }
+      });
+    }
+
+
+   
+
+    showAlert(message) {
+      Swal.fire({
+        title: message,
+        icon: 'success',
+        customClass: {
+          confirmButton: 'btn btn-success'
+        },
+        buttonsStyling: false
+      });
     }
 }
 
-const productos = [
-
-    new Producto(1, 'Branding basico', 7000 , 'El branding basico incluye logo y paleta de colores para tu marca'),
-    new Producto(2, 'Asesorias', 1500 , 'Te asesoramos sobre marketing digital'),
-    new Producto(3, 'Gestion de RRSS', 13000, 'Gestion de cualquier red social'),
-    new Producto(4, 'Posicionamiento en el mercado', 9000 , 'Posicionamiento en el mercado'),
-    new Producto(5, 'Branding Full', 35 , 'Bringamos un asesoramiento completo para el re lanzamiento de tu marca')
-];
-
-// console.log(productos);
-// tomo los productos del div con id = product-containter
-const productContainer = document.querySelector('#product-container');
-
- 
-// recorro el arreglo de productos y creo las cartas para mostrar en el front
-productos.forEach(producto => {
-
-  const productDiv = document.createElement('div');
- 
-  productDiv.classList.add("producto");
-
-  productDiv.style.backgroundColor='#FFFFFF';
-
-  //innerHTML
-
-  productDiv.innerHTML = `
-
-    <div class='card'>
-
-      <h3>${producto.id}</h3>
-
-      <h1>${producto.nombre}</h1>
-
-      <p>${producto.precio}</p>
-
-      <p>${producto.description}</p>
-
-      <button class="agregar-carrito">Agregar al Carrito</button>
-      <button class="eliminar-carrito">Eliminar del Carrito</button>
-
-    </div>
-
-  `;
 
 
-  productContainer.appendChild(productDiv);
 
-  const addButton = productDiv.querySelector('.agregar-carrito');
-  addButton.addEventListener('click', () => {
-    console.log("Se agrego al carrito!", producto.nombre);
-    addCarrito(producto);
-  });
+const tiendaSofi = new TiendaSofi();
 
-
-  const deleteButton = productDiv.querySelector('.eliminar-carrito');
-  addButton.addEventListener('click', () => {
-    console.log("Se saco del carrito!", producto.nombre);
-    eliminarCarrito(producto.id);
-  });
-})
-
-
+//----------------------------------------------------------------
+/*
 let productosEnCarrito = [];
 
 function addCarrito (producto) {
@@ -100,4 +155,4 @@ function eliminarCarrito(id) {
     console.log("El producto no existe en el carrito.");
   }
 }
-
+*/
